d1bc02a203f62db19ea08ff7df6cdb62fa7dd8a4
Kevin Jesús Escutia Ceja <kevinjec115@gmail.com>
2022-08-16 11:08:51 -0500

    Avance de las clases de android

--- app/src/main/java/com/example/cvsx/Formulario2.java
+++ app/src/main/java/com/example/cvsx/Formulario2.java
@@ -2,13 +2,26 @@ package com.example.cvsx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Formulario2 extends AppCompatActivity {
 
+    Button btnSalir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario2);
+
+        btnSalir = findViewById(R.id.btnFSalir);
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Formulario2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cvsx/MainActivity.java
+++ app/src/main/java/com/example/cvsx/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cvsx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,11 +10,23 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnAcceso;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "En OnStart", Toast.LENGTH_SHORT).show();
+        btnAcceso = findViewById(R.id.btnMMostrar);
+        btnAcceso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Formulario2.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
     @Override
